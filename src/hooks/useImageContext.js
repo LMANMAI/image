e9f0e6e860e360b5019fe1171804ddc55917455a
Lucas Manuel <lucas_sknr.010@live.com>
2020-12-19 00:39:05 -0300

    termine el paginador y agregue el efecto para que suba automaticamente la pag

--- src/hooks/useImageContext.js
+++ src/hooks/useImageContext.js
@@ -2,31 +2,46 @@ import React, {useState, createContext, useEffect} from 'react';
 
 export const ImageContext = createContext();
 const ImageProvider = (props) =>{
-
+    //states del Context
     const [ state, setState ] = useState('');
     const [ result, setResult ] = useState([]);
-    const [historial, setHistorial ] = useState([]);
+    const [ historial, setHistorial ] = useState([]);
     //states del paginador
     const [pages, setPages ] = useState(1);
     const [totalpaages, setTotalPages ] = useState(1)
+    //state del hastag
+    const [tag, setTagSearch ] = useState('');
+
     //hacer la consulta a la api 
     useEffect(()=>{
         if(state === '') return;
-        const consultaApi = async () =>{
-            const porPag = 15;
-            const apiKEY = `19481418-e1589af79378122d7dd026a5d`;
-            const url = `https://pixabay.com/api/?key=${apiKEY}&q=${state}&per_page=${porPag}`;
-            const consulta = await fetch(url);
-            const resultado = await consulta.json();
-            setResult(resultado.hits);
-            setTotalPages(Math.ceil(resultado.totalHits / porPag))
-
-        }
+            const consultaApi = async () =>{
+                const porPag = 15;
+                const apiKEY = `19481418-e1589af79378122d7dd026a5d`;
+                const url = `https://pixabay.com/api/?key=${apiKEY}&q=${state}&per_page=${porPag}&page=${pages}`;
+                const consulta = await fetch(url);
+                const resultado = await consulta.json();
+                setResult(resultado.hits);
+                setTotalPages(Math.ceil(resultado.totalHits / porPag));
+            }
+            
         consultaApi();
-        historial.push(state);
-    }, [state]);
-    
- 
+        const wrap = document.querySelector('.wrap');
+        wrap.scrollIntoView({behavior: 'smooth'});
+        
+    }, [state,pages]);
+
+    const paginaAnterior = ()=>{
+        const setpaginaactual = pages -1;
+        if(setpaginaactual === 0) return;
+        setPages(setpaginaactual);
+    }
+    const paginaSiguiente = ()=>{
+        const setpaginaactual = pages + 1;
+        if(setpaginaactual > totalpaages) return;
+        setPages(setpaginaactual);
+             }
+   
     return(
         <ImageContext.Provider
             value={{
@@ -34,7 +49,12 @@ const ImageProvider = (props) =>{
                 state,
                 result,
                 historial,
-                setState
+                pages,
+                totalpaages,
+                setState,
+                paginaAnterior,
+                paginaSiguiente,
+                setTagSearch
             }}
         >
             {props.children}
